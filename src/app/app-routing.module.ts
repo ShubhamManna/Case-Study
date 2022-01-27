dd7b2476c8e5b45c1c0619a314538bfaab8a6522
ShubhamManna <manna.shubhm@gmail.com>
2022-01-27 17:16:02 +0530

    updated re-routing and added AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginModuleComponent } from './components/login-module/login-module.component';
+import { RegisterModuleComponent } from './components/register-module/register-module.component';
+import { UserComponent } from './components/user/user.component';
+import { AuthService } from './service/auth.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent, canActivate:[AuthService]},
+  { path: 'login', component: LoginModuleComponent},
+  { path: 'register', component: RegisterModuleComponent},
+  { path: '', component: LoginModuleComponent},
+  { path: 'user', component: UserComponent, canActivate:[AuthService]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArtistCornerComponent } from './components/artist-corner/artist-corner.component';
+import { LoginModuleComponent } from './components/login-module/login-module.component';
+import { RegisterModuleComponent } from './components/register-module/register-module.component';
+import { UserComponent } from './components/user/user.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtistCornerComponent,
+    LoginModuleComponent,
+    RegisterModuleComponent,
+    UserComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
